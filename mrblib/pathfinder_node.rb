f17416c93083831b10e065fedc8e52a5219d4530
Giovanni Sakti <giosakti@gmail.com>
2019-04-03 11:21:41 +0700

    add method for fetching all scheduled containers in a particular node

--- mrblib/pathfinder_node.rb
+++ mrblib/pathfinder_node.rb
@@ -18,19 +18,33 @@ module Pathfinder
       res = @client.request(
         'POST',
         '/api/v1/node/register',
-        craft_request_body(payload)
+        craft_request_body(payload: payload)
+      )
+      return res
+    end
+
+    def get_scheduled_containers(cluster_name:, authentication_token:)
+      payload = {
+        cluster_name: cluster_name
+      }.to_json
+      res = @client.request(
+        'GET',
+        '/api/v1/node/containers/scheduled',
+        craft_request_body(payload: payload, authentication_token: authentication_token)
       )
       return res
     end
 
     private
 
-    def craft_request_body(payload)
-      {
+    def craft_request_body(payload:, authentication_token: nil)
+      req = {
         'Body' => payload,
         'Content-Type' => 'application/json; charset=utf-8',
         'Content-Length' => payload.length 
       }
+      req['X-Auth-Token'] = authentication_token unless authentication_token.nil?
+      req
     end
   end
 end
--- test/pathfinder_node_test.rb
+++ test/pathfinder_node_test.rb
@@ -10,12 +10,32 @@ module Pathfinder
       }
     end
 
+    def register
+      pathfinder_node = PathfinderNode.new(port: 3000)
+      response = pathfinder_node.register(
+        cluster_name: shared_vars[:cluster_name],
+        cluster_password: shared_vars[:cluster_password],
+        node: shared_vars[:node]
+      )
+      JSON.parse(response.body)['data']['authentication_token']
+    end
+
     def test_register_success
       pathfinder_node = PathfinderNode.new(port: 3000)
       response = pathfinder_node.register(
         cluster_name: shared_vars[:cluster_name],
         cluster_password: shared_vars[:cluster_password],
         node: shared_vars[:node]
+      )
+      assert_equal(200, response.code)
+    end
+
+    def test_get_scheduled_containers_success
+      pathfinder_node = PathfinderNode.new(port: 3000)
+      token = register
+      response = pathfinder_node.get_scheduled_containers(
+        cluster_name: shared_vars[:cluster_name],
+        authentication_token: token
       )
       assert_equal(200, response.code)
     end

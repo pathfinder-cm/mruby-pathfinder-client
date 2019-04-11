a8a2a1e88c12607d1d43707f1c1c293f52a633f5
Giovanni Sakti <giosakti@gmail.com>
2019-04-12 04:29:37 +0700

    also put cluster name and authentication token as instance variable for pathfinder node

--- mrblib/pathfinder_node.rb
+++ mrblib/pathfinder_node.rb
@@ -3,15 +3,15 @@ require(File.expand_path('container', File.dirname(__FILE__)))
 
 module Pathfinder
   class PathfinderNode
-    def initialize(opts = {})
-      opts[:scheme] ||= 'http'
-      opts[:address] ||= 'localhost'
-      @client = SimpleHttp.new(opts[:scheme], opts[:address], opts[:port])
+    def initialize(cluster_name:, authentication_token: '', scheme: 'http', address: 'localhost', port: 80)
+      @cluster_name = cluster_name
+      @authentication_token = authentication_token
+      @client = SimpleHttp.new(scheme, address, port)
     end
 
-    def register(cluster_name:, cluster_password:, node:)
+    def register(cluster_password:, node:)
       payload = {
-        cluster_name: cluster_name,
+        cluster_name: @cluster_name,
         password: cluster_password,
         node_hostname: node.hostname,
         node_ipaddress: node.ipaddress
@@ -22,20 +22,21 @@ module Pathfinder
         craft_request_body(payload: payload)
       )
       if res.code == 200
-        return true, JSON.parse(res.body)['data']['authentication_token']
+        @authentication_token = JSON.parse(res.body)['data']['authentication_token']
+        return true, @authentication_token
       else
         return false, nil
       end
     end
 
-    def get_scheduled_containers(cluster_name:, authentication_token:)
+    def get_scheduled_containers
       payload = {
-        cluster_name: cluster_name
+        cluster_name: @cluster_name
       }.to_json
       res = @client.request(
         'GET',
         '/api/v1/node/containers/scheduled',
-        craft_request_body(payload: payload, authentication_token: authentication_token)
+        craft_request_body(payload: payload, authentication_token: @authentication_token)
       )
       if res.code == 200
         items = JSON.parse(res.body)['data']['items']
@@ -51,16 +52,16 @@ module Pathfinder
       end
     end
 
-    def update_ipaddress(cluster_name:, authentication_token:, container:)
+    def update_ipaddress(container:)
       payload = {
-        cluster_name: cluster_name,
+        cluster_name: @cluster_name,
         hostname: container.hostname,
         ipaddress: container.ipaddress
       }.to_json
       res = @client.request(
         'POST',
         '/api/v1/node/containers/ipaddress',
-        craft_request_body(payload: payload, authentication_token: authentication_token)
+        craft_request_body(payload: payload, authentication_token: @authentication_token)
       )
       if res.code == 200
         return true
@@ -69,15 +70,15 @@ module Pathfinder
       end
     end
 
-    def mark_container_as_provisioned(cluster_name:, authentication_token:, container:)
+    def mark_container_as_provisioned(container:)
       payload = {
-        cluster_name: cluster_name,
+        cluster_name: @cluster_name,
         hostname: container.hostname
       }.to_json
       res = @client.request(
         'POST',
         '/api/v1/node/containers/mark_provisioned',
-        craft_request_body(payload: payload, authentication_token: authentication_token)
+        craft_request_body(payload: payload, authentication_token: @authentication_token)
       )
       if res.code == 200
         item = JSON.parse(res.body)['data']
@@ -93,15 +94,15 @@ module Pathfinder
       end
     end
 
-    def mark_container_as_provision_error(cluster_name:, authentication_token:, container:)
+    def mark_container_as_provision_error(container:)
       payload = {
-        cluster_name: cluster_name,
+        cluster_name: @cluster_name,
         hostname: container.hostname
       }.to_json
       res = @client.request(
         'POST',
         '/api/v1/node/containers/mark_provision_error',
-        craft_request_body(payload: payload, authentication_token: authentication_token)
+        craft_request_body(payload: payload, authentication_token: @authentication_token)
       )
       if res.code == 200
         item = JSON.parse(res.body)['data']
@@ -117,15 +118,15 @@ module Pathfinder
       end
     end
 
-    def mark_container_as_deleted(cluster_name:, authentication_token:, container:)
+    def mark_container_as_deleted(container:)
       payload = {
-        cluster_name: cluster_name,
+        cluster_name: @cluster_name,
         hostname: container.hostname
       }.to_json
       res = @client.request(
         'POST',
         '/api/v1/node/containers/mark_deleted',
-        craft_request_body(payload: payload, authentication_token: authentication_token)
+        craft_request_body(payload: payload, authentication_token: @authentication_token)
       )
       if res.code == 200
         item = JSON.parse(res.body)['data']
@@ -141,9 +142,9 @@ module Pathfinder
       end
     end
 
-    def store_metrics(cluster_name:, authentication_token:, metrics:)
+    def store_metrics(metrics:)
       payload = {
-        cluster_name: cluster_name,
+        cluster_name: @cluster_name,
         memory: {
           free: metrics[:memory][:free],
           used: metrics[:memory][:used],
@@ -153,7 +154,7 @@ module Pathfinder
       res = @client.request(
         'POST',
         '/api/v1/node/nodes/store_metrics',
-        craft_request_body(payload: payload, authentication_token: authentication_token)
+        craft_request_body(payload: payload, authentication_token: @authentication_token)
       )
       if res.code == 200
         return true
--- test/pathfinder_node_test.rb
+++ test/pathfinder_node_test.rb
@@ -2,9 +2,16 @@ require(File.expand_path('../mrblib/pathfinder_node', File.dirname(__FILE__)))
 
 module Pathfinder
   class PathfinderNodeTest < MTest::Unit::TestCase
+    def pathfinder_node
+      @pathfinder_node ||= PathfinderNode.new(
+        cluster_name: 'default', 
+        authentication_token: 'pathfinder',
+        port: 3000
+      )
+    end
+
     def shared_vars
       {
-        cluster_name: 'default',
         cluster_password: 'pathfinder',
         node: Node.new(hostname: 'test-01', ipaddress: '127.0.0.1'),
         container: Container.new(hostname: 'test-01', ipaddress: '127.0.0.1'),
@@ -19,9 +26,7 @@ module Pathfinder
     end
 
     def register
-      pathfinder_node = PathfinderNode.new(port: 3000)
       ok, authentication_token = pathfinder_node.register(
-        cluster_name: shared_vars[:cluster_name],
         cluster_password: shared_vars[:cluster_password],
         node: shared_vars[:node]
       )
@@ -29,9 +34,7 @@ module Pathfinder
     end
 
     def test_register_success
-      pathfinder_node = PathfinderNode.new(port: 3000)
       ok, _ = pathfinder_node.register(
-        cluster_name: shared_vars[:cluster_name],
         cluster_password: shared_vars[:cluster_password],
         node: shared_vars[:node]
       )
@@ -39,22 +42,15 @@ module Pathfinder
     end
 
     def test_get_scheduled_containers_success
-      pathfinder_node = PathfinderNode.new(port: 3000)
-      token = register
-      ok, _ = pathfinder_node.get_scheduled_containers(
-        cluster_name: shared_vars[:cluster_name],
-        authentication_token: token
-      )
+      register
+      ok, _ = pathfinder_node.get_scheduled_containers
       assert_equal(true, ok)
     end
 
     # NOTE: test node must already have assigned container and is not 'DELETED'
     def test_update_ipaddress_success
-      pathfinder_node = PathfinderNode.new(port: 3000)
-      token = register
+      register
       ok = pathfinder_node.update_ipaddress(
-        cluster_name: shared_vars[:cluster_name],
-        authentication_token: token,
         container: shared_vars[:container]
       )
       assert_equal(true, ok)
@@ -62,11 +58,8 @@ module Pathfinder
 
     # NOTE: test node must already have assigned container with status: 'SCHEDULED'
     def test_mark_container_as_provisioned_success
-      pathfinder_node = PathfinderNode.new(port: 3000)
-      token = register
+      register
       ok, _ = pathfinder_node.mark_container_as_provisioned(
-        cluster_name: shared_vars[:cluster_name],
-        authentication_token: token,
         container: shared_vars[:container]
       )
       assert_equal(true, ok)
@@ -74,11 +67,8 @@ module Pathfinder
 
     # NOTE: test node must already have assigned container with status: 'SCHEDULED'
     def test_mark_container_as_provision_error_success
-      pathfinder_node = PathfinderNode.new(port: 3000)
-      token = register
+      register
       ok, _ = pathfinder_node.mark_container_as_provision_error(
-        cluster_name: shared_vars[:cluster_name],
-        authentication_token: token,
         container: shared_vars[:container]
       )
       assert_equal(true, ok)
@@ -86,22 +76,16 @@ module Pathfinder
 
     # NOTE: test node must already have assigned container with status: 'SCHEDULE_DELETION'
     def test_mark_container_as_deleted_success
-      pathfinder_node = PathfinderNode.new(port: 3000)
-      token = register
+      register
       ok, _ = pathfinder_node.mark_container_as_deleted(
-        cluster_name: shared_vars[:cluster_name],
-        authentication_token: token,
         container: shared_vars[:container]
       )
       assert_equal(true, ok)
     end
 
     def test_store_metrics_success
-      pathfinder_node = PathfinderNode.new(port: 3000)
-      token = register
+      register
       ok = pathfinder_node.store_metrics(
-        cluster_name: shared_vars[:cluster_name],
-        authentication_token: token,
         metrics: shared_vars[:metrics]
       )
       assert_equal(true, ok)
